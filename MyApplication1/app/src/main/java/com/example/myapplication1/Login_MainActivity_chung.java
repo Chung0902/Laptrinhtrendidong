d1596a94b4830bd64b17dfe665ea11b7417e4f9d
NGUYEN THI CHUNG <chungnguyen.130902@gmail.com>
2022-08-27 15:33:43 +0700

    first commit

---
+++ MyApplication1/app/src/main/java/com/example/myapplication1/Login_MainActivity_chung.java
@@ -0,0 +1,14 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_MainActivity_chung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_main_chung);
+    }
+}
