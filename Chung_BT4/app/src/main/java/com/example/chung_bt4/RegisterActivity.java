a8d1f615c7c2dc12a574366d5305658f837f3647
NGUYEN THI CHUNG <chungnguyen.130902@gmail.com>
2022-09-13 22:22:04 +0700

    BT4

--- Chung_BT4/app/src/main/java/com/example/chung_bt4/RegisterActivity.java
+++ Chung_BT4/app/src/main/java/com/example/chung_bt4/RegisterActivity.java
@@ -5,26 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        TextView btn = findViewById(R.id.alreadyHaveAccount);
-        btn.setOnClickListener(new View.OnClickListener() {
+        btnRegister = (Button) findViewById(R.id.buttonRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
-                startActivities(intent);
-                //startActivities(new Intent(RegisterActivity.this,LoginActivity.class));
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
-
-            private void startActivities(Intent intent) {
-            }
-
         });
     }
    
