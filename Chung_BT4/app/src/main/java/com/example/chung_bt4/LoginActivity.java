5f0bbf145f4d225ec856501dde1d7f5a86b006de
NGUYEN THI CHUNG <chungnguyen.130902@gmail.com>
2022-09-13 21:30:17 +0700

    bai_tap_4

---
+++ Chung_BT4/app/src/main/java/com/example/chung_bt4/LoginActivity.java
@@ -0,0 +1,31 @@
+package com.example.chung_bt4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        TextView btn = findViewById(R.id.textViewSignup);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivities(intent);
+                //startActivities(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+
+            private void startActivities(Intent intent) {
+            }
+
+        });
+    }
+    
+}
