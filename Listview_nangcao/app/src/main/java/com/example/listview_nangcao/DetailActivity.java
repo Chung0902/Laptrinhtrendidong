c84926791ef77f7e93a5eb71ef8442891d933e92
NGUYEN THI CHUNG <chungnguyen.130902@gmail.com>
2022-09-20 23:07:11 +0700

    listview_nangcao

---
+++ Listview_nangcao/app/src/main/java/com/example/listview_nangcao/DetailActivity.java
@@ -0,0 +1,10 @@
+package com.example.listview_nangcao;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class DetailActivity {
+    public DetailActivity(Context applicationContext, int simple_list_item_1, List<String> list) {
+    }
+}
