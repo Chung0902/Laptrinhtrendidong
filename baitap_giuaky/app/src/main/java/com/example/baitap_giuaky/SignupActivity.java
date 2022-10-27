a5a4786c81a259b4f29b83e0c051eb972772731a
NGUYEN THI CHUNG <chungnguyen.130902@gmail.com>
2022-10-27 23:15:56 +0700

    baitapck

--- baitap_giuaky/app/src/main/java/com/example/baitap_giuaky/SignupActivity.java
+++ baitap_giuaky/app/src/main/java/com/example/baitap_giuaky/SignupActivity.java
@@ -6,23 +6,55 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignupActivity extends AppCompatActivity {
 
+    EditText username, password, repassword;
     private Button btnsignup;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        username = (EditText) findViewById(R.id.editTextUsername);
+        password = (EditText) findViewById(R.id.editTexPass);
+        repassword = (EditText) findViewById(R.id.editTexConformPassword);
         btnsignup = (Button) findViewById(R.id.buttonRegister);
+        DB=new DBHelper(this);
 
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-                startActivity(intent);
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repassword.getText().toString();
+
+                if(user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(SignupActivity.this, "Vui lòng nhập tất cả các trường", Toast.LENGTH_SHORT).show();
+                else{
+                    if(pass.equals(repass)){
+                        Boolean checkuser = DB.checkusername(user);
+                        if(checkuser==false){
+                            Boolean insert = DB.insertData(user, pass);
+                            if(insert==true){
+                                Toast.makeText(SignupActivity.this, "Đã đăng ký thành công", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(SignupActivity.this, "đăng ký không thành công", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            Toast.makeText(SignupActivity.this, "Người dùng đã tồn tại! Vui lòng đăng nhập", Toast.LENGTH_SHORT).show();
+                        }
+                    }else{
+                        Toast.makeText(SignupActivity.this, "Mật khẩu không khớp", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
 
